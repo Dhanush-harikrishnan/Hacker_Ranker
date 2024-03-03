b3f3b83f1a9f690fe52254d8d626c44ee1b244b2
Dhanush H <dhanushhari150504@gmail.com>
2024-03-03 09:48:10 +0530

    Create Left_rotation.py

---
+++ Left_rotation.py
@@ -0,0 +1,27 @@
+import os
+
+def rotateLeft(d, arr):
+    n = len(arr)
+    d %= n
+
+    rotated_list = arr[d:] + arr[:d]
+
+    return rotated_list
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
